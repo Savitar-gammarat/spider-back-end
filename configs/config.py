8600fc6610994d30a6609bc99dc1aa31b9fa7c68
weekbin <18717744742@163.com>
2019-01-19 01:14:53 +0800

    add a secret key

--- configs/config.py
+++ configs/config.py
@@ -1,10 +1,13 @@
-
 """
 the path of DataBase
 """
-
 DB_USER = "root"
 DB_PASSWORD = "991004"
 DB_HOST = "47.101.196.53"
 DB_PORT = "3306"
 DB_SCHEMA = "techurls"
+
+"""
+SECRET_KEY, something important!
+"""
+SECRET_KEY = "Trump-eat-chicken"
