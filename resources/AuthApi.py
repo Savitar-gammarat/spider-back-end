0fbb3bfcc2ec4f22f40f723bffbd3a909bd1f087
weekbin <18717744742@163.com>
2019-01-21 17:10:12 +0800

    update the post method in AuthApi

--- resources/AuthApi.py
+++ resources/AuthApi.py
@@ -2,7 +2,8 @@ from flask import request, g
 from flask_httpauth import HTTPTokenAuth
 from flask_restful import Resource
 from models.User import User
-from configs.database import to_dict
+from configs.database import to_dict, db
+from datetime import datetime
 
 auth = HTTPTokenAuth()
 
@@ -28,13 +29,18 @@ class AuthAPI(Resource):
         Login in
         :return: token or error message
         """
-        username = request.json.get("username")
-        password = request.json.get("password")
+        try:
+            username = request.json.get("username")
+            password = request.json.get("password")
+        except KeyError:
+            return {"error": "lack necessary arguments!"}, 406
         user = User.query.filter_by(username=username).first()
         if user is None:
             return {"error": "Username doesn't exist"}, 404
         if not user.verify_password(password):
             return {"error": "Wrong password"}, 403
+        user.last_login_time = datetime.now()
+        db.session.commit()
         token = user.generate_auth_token()
         user = to_dict(user)
         if user["customization"] is None:
